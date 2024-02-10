bd4435ecc2d1ec07cc8ed68bdfb14ff0847b63d3
f1v3-dev <f1v3@kakao.com>
2024-02-10 21:57:53 +0900

    [FEAT] 포인트 정책 페이지 초안 구현

--- src/main/java/store/ckin/front/pointpolicy/service/impl/PointPolicyServiceImpl.java
+++ src/main/java/store/ckin/front/pointpolicy/service/impl/PointPolicyServiceImpl.java
@@ -3,7 +3,7 @@ package store.ckin.front.pointpolicy.service.impl;
 import store.ckin.front.pointpolicy.service.PointPolicyService;
 
 /**
- * {class name}.
+ * 포인트 정책 서비스를 구현한 클래스입니다.
  *
  * @author 정승조
  * @version 2024. 02. 08.
