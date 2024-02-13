950c3b109391a5fd43a0692cc5083703007b7285
f1v3-dev <f1v3@kakao.com>
2024-02-13 17:06:44 +0900

    [REFACTOR] 포인트 정책 컨트롤러 수정

--- src/main/java/store/ckin/front/pointpolicy/controller/PointPolicyController.java
+++ src/main/java/store/ckin/front/pointpolicy/controller/PointPolicyController.java
@@ -1,8 +1,13 @@
 package store.ckin.front.pointpolicy.controller;
 
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import store.ckin.front.pointpolicy.dto.request.CreatePointPolicyRequestDto;
+import store.ckin.front.pointpolicy.service.PointPolicyService;
 
 /**
  * 포인트 정책 페이지를 호출하는 컨트롤러입니다.
@@ -12,11 +17,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/admin/policy/point")
 public class PointPolicyController {
+
+    private final PointPolicyService pointPolicyService;
 
     @GetMapping
     public String getPointPolicyMain() {
         return "admin/point-policy/main";
     }
+
+    @GetMapping("/register")
+    public String getPointPolicyRegister() {
+        return "admin/point-policy/register";
+    }
+
+    @PostMapping
+    public String createPointPolicyRegister(@Valid CreatePointPolicyRequestDto createPointPolicy) {
+
+        pointPolicyService.createPointPolicy(createPointPolicy);
+        return "redirect:/admin/policy/point";
+    }
+
+
 }
