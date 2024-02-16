3df911f15c1b0d77ce06e0c91313317463867266
dduneon <dduneon@gmail.com>
2024-02-16 16:30:39 +0900

    [ADD] Test Class 생성

---
+++ src/test/java/store/ckin/front/tag/adapter/impl/TagAdaptorImplTest.java
@@ -0,0 +1,30 @@
+package store.ckin.front.tag.adapter.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * description
+ *
+ * @author 김준현
+ * @version 2024. 02. 16
+ */
+class TagAdaptorImplTest {
+
+    @Test
+    void insertTag() {
+    }
+
+    @Test
+    void selectTagList() {
+    }
+
+    @Test
+    void updateTag() {
+    }
+
+    @Test
+    void deleteTag() {
+    }
+}
