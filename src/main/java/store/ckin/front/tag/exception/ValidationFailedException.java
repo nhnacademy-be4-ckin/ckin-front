6b2ae1538550971d8884c8349e427b6447e9b828
dduneon <dduneon@gmail.com>
2024-02-16 19:29:31 +0900

    [ADD] Custom Exception 추가

---
+++ src/main/java/store/ckin/front/tag/exception/ValidationFailedException.java
@@ -0,0 +1,10 @@
+package store.ckin.front.tag.exception;
+
+/**
+ * description
+ *
+ * @author 김준현
+ * @version 2024. 02. 16
+ */
+public class ValidationFailedException extends RuntimeException{
+}
--- src/test/java/store/ckin/front/tag/adapter/impl/TagAdaptorImplTest.java
+++ src/test/java/store/ckin/front/tag/adapter/impl/TagAdaptorImplTest.java
@@ -2,7 +2,9 @@ package store.ckin.front.tag.adapter.impl;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import store.ckin.front.tag.adapter.TagAdaptor;
 
 /**
  * description
@@ -10,7 +12,9 @@ import org.junit.jupiter.api.Test;
  * @author 김준현
  * @version 2024. 02. 16
  */
+@RequiredArgsConstructor
 class TagAdaptorImplTest {
+    private final TagAdaptor tagAdaptor;
 
     @Test
     void insertTag() {
