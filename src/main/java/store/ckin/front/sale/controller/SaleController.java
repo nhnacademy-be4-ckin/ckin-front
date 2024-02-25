2e0eee9763f98615aa597d0266ee4e30c3be9493
f1v3-dev <f1v3@kakao.com>
2024-02-25 22:37:50 +0900

    [FEAT] 주문 - 상품 & 배송지 등록

---
+++ src/main/java/store/ckin/front/sale/controller/SaleController.java
@@ -0,0 +1,24 @@
+package store.ckin.front.sale.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 주문 컨트롤러 클래스입니다.
+ *
+ * @author 정승조
+ * @version 2024. 02. 25.
+ */
+
+@Controller
+@RequestMapping("/sale")
+@RequiredArgsConstructor
+public class SaleController {
+
+    @GetMapping
+    public String getSale() {
+        return "sale/main";
+    }
+}
