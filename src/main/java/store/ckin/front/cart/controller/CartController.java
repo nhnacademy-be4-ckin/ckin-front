617389bc6e45a1bbf864228588e48c245b51648e
dduneon <dduneon@gmail.com>
2024-02-27 16:52:57 +0900

    [ADD] 초기 구조 생성

--- src/main/java/store/ckin/front/cart/controller/CartController.java
+++ src/main/java/store/ckin/front/cart/controller/CartController.java
@@ -1,10 +1,20 @@
 package store.ckin.front.cart.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * description
  *
  * @author 김준현
  * @version 2024. 02. 27
  */
+@Controller
+@RequestMapping("/cart")
 public class CartController {
+    @GetMapping
+    public String cartPage() {
+        return "cart/index";
+    }
 }
