e56be10f11a5512f0dd122e9090e0106d21101aa
f1v3-dev <f1v3@kakao.com>
2024-02-27 20:52:57 +0900

    [REFACTOR] 쿠폰 조회 스크립트 변경

--- src/main/resources/static/js/coupon.js
+++ src/main/resources/static/js/coupon.js
@@ -1,21 +1,62 @@
 let memberId = 1;
 
-let bookId = [];
-bookId.push(1);
-bookId.push(2);
+let bookIds = [];
+bookIds.push(1);
+bookIds.push(2);
+
+console.log(document.getElementById('couponBtn').value);
+
+let couponMap = new Map();
+// 페이지가 로드되면 memberId와 bookId를 통해 사용 가능한 쿠폰 리스트를 받아온다.
+$(document).ready(function () {
+    $.ajax({
+        url: '/sale/coupon?memberId=' + memberId + '&' + bookIds.map(id => 'bookId=' + id).join('&'),
+        method: 'GET',
+        success: function (coupons) {
+            coupons.forEach(coupon => {
+                couponMap.set(coupon.id, coupon);
+            });
+            console.log(couponMap);
+        },
+        error: function (xhr, status, error) {
+            console.error('Request failed with status', status, error);
+        }
+    });
+});
+
+document.getElementById('couponBtn').addEventListener('click', function () {
+    let bookId = document.getElementById('couponBtn').value;
+    console.log('couponBtn clicked');
+    couponMap.forEach(value => {
+        renderCoupon(value);
+    });
+})
+
+function renderCoupon(coupon) {
+    let couponTableBody = document.getElementById('couponTable').getElementsByTagName('tbody')[0];
+    coupon.forEach(function (coupon) {
+        let row =
+            '<tr>' +
+            '<td><input type="radio" name="coupon" value="' + coupon.id + '"></td>' +
+            '<td>' + coupon.name + '</td>' +
+            '<td style="color: dodgerblue">' + coupon.discountRate + '%</td>' +
+            '<td style="color: dodgerblue">' + coupon.discountPrice + '원</td>' +
+            '</tr>';
+        couponTableBody.append(row);
+    });
+
+    console.log('renderCoupons');
+}
+
+
+// 사용자가 쿠폰 적용 버튼을 누르면 해당 bookId에 대한 적용 가능한 쿠폰 리스트를 빼서 화면에 보여준다.
+
+// 쿠폰 적용 버튼을 누르면 해당 bookId에 대한 적용 가능한 쿠폰 리스트를 빼서 화면에 보여준다.
 
 $(document).ready(function () {
-    $('#couponBtn').click(function () {
-        $.ajax({
-            url: '/sale/coupon?memberId=' + memberId + '&' + bookId.map(id => 'bookId=' + id).join('&'),
-            method: 'GET',
-            success: function (coupons) {
-                renderCoupons(coupons, bookId);
-            },
-            error: function (xhr, status, error) {
-                console.error('Request failed with status', status, error);
-            }
-        });
+    $('#couponBtn').click(function (id) {
+        console.log('renderCoupons');
+        renderCoupons(couponMap, id);
     });
 
     $('#selectCouponBtn').click(function () {
@@ -39,52 +80,39 @@ $(document).ready(function () {
     });
 });
 
-function renderCoupons(coupons, bookId) {
-    let couponTableBody = $('#couponTable tbody');
-    couponTableBody.empty();
-
-    // 선택 가능한 쿠폰과 선택 불가능한 쿠폰을 담을 배열을 초기화합니다.
-    let selectableCoupons = [];
-    let disabledCoupons = [];
-
-    $.each(coupons, function (index, coupon) {
-        console.log("coupon = " + coupon);
-        if (coupon.couponTemplateId !== 1 && coupon.bookId !== bookId) {
-            console.log('not append coupon : ' + coupon);
-            // 선택 불가능한 쿠폰은 disabledCoupons 배열에 추가합니다.
-            disabledCoupons.push(coupon);
-        } else if (coupon.couponTemplateId === 1 || coupon.bookId === bookId) {
-            console.log('append coupon : ' + coupon);
-            console.log(coupon);
-            // 선택 가능한 쿠폰은 selectableCoupons 배열에 추가합니다.
-            selectableCoupons.push(coupon);
-        }
-    });
-
-    // 선택 가능한 쿠폰을 먼저 추가합니다.
-    selectableCoupons.forEach(function (coupon) {
-        let row =
-            '<tr>' +
-            '<td><input type="radio" name="coupon" value="' + coupon.id + '"></td>' +
-            '<td>' + coupon.name + '</td>' +
-            '<td style="color: dodgerblue">' + coupon.discountRate + '%</td>' +
-            '<td style="color: dodgerblue">' + coupon.discountPrice + '원</td>' +
-            '</tr>';
-        couponTableBody.append(row);
-    });
-
-    // 선택 불가능한 쿠폰을 추가합니다.
-    disabledCoupons.forEach(function (coupon) {
-        let row =
-            '<tr>' +
-            '<td><input type="radio" name="coupon" value="' + coupon.id + '" disabled></td>' +
-            '<td>' + coupon.name + '</td>' +
-            '<td style="color: dodgerblue">' + coupon.discountRate + '%</td>' +
-            '<td style="color: dodgerblue">' + coupon.discountPrice + '원</td>' +
-            '</tr>';
-        couponTableBody.append(row);
-    });
-}
+// function renderCoupons(couponMap, bookId) {
+//     let couponTableBody = $('#couponTable tbody');
+//     couponTableBody.empty();
+//
+//     let selectableCoupons = [];
+//
+//     $.each(couponMap, function (index, coupon) {
+//         console.log("coupon = " + coupon);
+//
+//         if (coupon.couponTemplateId !== 1 && coupon.bookId !== bookId) {
+//             console.log('not append coupon : ' + coupon);
+//             // 선택 불가능한 쿠폰은 disabledCoupons 배열에 추가합니다.
+//             disabledCoupons.push(coupon);
+//         } else if (coupon.couponTemplateId === 1 || coupon.bookId === bookId) {
+//             console.log('append coupon : ' + coupon);
+//             console.log(coupon);
+//             // 선택 가능한 쿠폰은 selectableCoupons 배열에 추가합니다.
+//             selectableCoupons.push(coupon);
+//         }
+//     });
+//
+//     selectableCoupons.forEach(function (coupon) {
+//         let row =
+//             '<tr>' +
+//             '<td><input type="radio" name="coupon" value="' + coupon.id + '"></td>' +
+//             '<td>' + coupon.name + '</td>' +
+//             '<td style="color: dodgerblue">' + coupon.discountRate + '%</td>' +
+//             '<td style="color: dodgerblue">' + coupon.discountPrice + '원</td>' +
+//             '</tr>';
+//         couponTableBody.append(row);
+//     });
+//
+// }
 
 
 function displayDiscountInfo(discountInfo) {
