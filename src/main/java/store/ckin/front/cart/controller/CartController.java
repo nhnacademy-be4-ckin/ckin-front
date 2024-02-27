cbe0a84b67dbb6fb7633023756fa482c70c416ba
dduneon <dduneon@gmail.com>
2024-02-28 03:13:06 +0900

    [ADD] cookie 테스트를 위한 컨트롤러 추가

--- src/main/java/store/ckin/front/cart/controller/CartController.java
+++ src/main/java/store/ckin/front/cart/controller/CartController.java
@@ -1,7 +1,15 @@
 package store.ckin.front.cart.controller;
 
+import java.util.Objects;
+import java.util.UUID;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,11 +18,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author 김준현
  * @version 2024. 02. 27
  */
+@Slf4j
 @Controller
 @RequestMapping("/cart")
 public class CartController {
     @GetMapping
-    public String cartPage() {
+    public String getCartPage(@CookieValue(name = "CART_ID") Cookie cookie) {
+        log.info("saved cookie: {} {}", cookie.getName(), cookie.getValue());
+        return "cart/index";
+    }
+
+    @PostMapping
+    public String addCartItem() {
         return "cart/index";
     }
 }
