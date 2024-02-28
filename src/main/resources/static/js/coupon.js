2a92f46e510a359c0f91555b9698f7d7c18459bd
f1v3-dev <f1v3@kakao.com>
2024-02-28 16:41:58 +0900

    [FEAT] 주문 - 쿠폰 수정

--- src/main/resources/static/js/coupon.js
+++ src/main/resources/static/js/coupon.js
@@ -1,17 +1,13 @@
-
 // TODO 1) 회원 ID
-let memberId = 1;
-
-// TODO 2) 주문할 책 ID 목록
-let bookIds = [];
-bookIds.push(1);
-bookIds.push(2);
-
-
-
-let couponList = [];
+let memberId = 5;
 
 // 페이지가 로드되면 memberId와 bookId를 통해 사용 가능한 쿠폰 리스트를 받아옵니다.
+let bookIds = [];
+bookInfo.forEach(book => {
+    return bookIds.push(book.bookId);
+});
+
+let couponList = [];
 $(document).ready(function () {
     $.ajax({
         url: '/sale/coupon?memberId=' + memberId + '&' + bookIds.map(id => 'bookId=' + id).join('&'),
@@ -28,26 +24,31 @@ $(document).ready(function () {
 });
 
 // couponBtn 버튼 클릭 이벤트를 처리합니다.
-document.getElementById('couponBtn').addEventListener('click', function () {
 
-    // 쿠폰 적용할 책의 ID를 가져옵니다.
-    let bookId = document.getElementById('couponBtn').value;
+let couponButtons = document.querySelectorAll('.couponBtn');
+couponButtons.forEach(function (btn) {
+    btn.addEventListener('click', function () {
+        // 쿠폰 적용할 책의 ID를 가져옵니다.
+        let bookId = this.value;
+        let categoryIds = bookInfo.get(parseInt(bookId)).categoryIds;
 
-    // TODO 3) 쿠폰 적용할 책의 카테고리 ID 목록 (임시로 1, 2로 설정)
-    let categoryIds = [];
-    categoryIds.push(1);
-    categoryIds.push(2);
+        // 쿠폰 목록을 렌더링 합니다.
+        renderCoupon(couponList, bookId, categoryIds);
+    })
+})
 
-    // 쿠폰을 렌더링합니다.
-    renderCoupon(couponList, bookId, categoryIds);
-});
 
 function renderCoupon(couponList, bookId, categoryIds) {
 
-    let couponTableBody = document.getElementById('couponTable').getElementsByTagName('tbody')[0];
+    let tableId = 'couponTable-' + bookId;
+    let couponTableBody = document.getElementById(tableId).getElementsByTagName('tbody')[0];
 
+    console.log(couponTableBody);
+    // 쿠폰 목록을 비웁니다.
+    couponTableBody.innerHTML = '';
+
+    console.log(couponTableBody);
     let selectableCoupons = [];
-    let disabledCoupons = [];
 
     // 쿠폰을 분류합니다.
     couponList.forEach(coupon => {
@@ -60,18 +61,15 @@ function renderCoupon(couponList, bookId, categoryIds) {
             || ((coupon.typeId == 2 && coupon.bookId == bookId))
             || ((coupon.typeId == 3 && categoryIds.includes(coupon.categoryId)))) {
             selectableCoupons.push(coupon);
-            console.log(coupon);
-
-        } else {
-            disabledCoupons.push(coupon);
         }
     });
 
-    // 쿠폰 목록을 비웁니다.
-    couponTableBody.innerHTML = '';
 
     // 선택 가능한 쿠폰을 HTML로 변환하여 출력합니다.
     selectableCoupons.forEach(coupon => {
+
+        console.log('선택된 쿠폰 HTML 출력')
+        console.log(coupon);
 
         /**
          * coupon.couponCodeId === 1 : 정액할인
@@ -79,17 +77,24 @@ function renderCoupon(couponList, bookId, categoryIds) {
          *
          * 따라서, 1번이면 discountPrice를 표시하고, 2번이면 discountRate를 표시합니다.
          */
-        let row
-            = '<tr>'
-            + '<td><input type="radio" name="coupon" value="' + coupon.id + '"></td>'
-            + '<td>' + coupon.name + '</td>'
-            + '<td>' + (coupon.couponCodeId == 1 ? coupon.discountPrice + '원' +
-                '<br/> <span>' + '상품 가격이 최소 ' + coupon.minOrderPrice + '원 이상' + '</span>'
-                : coupon.discountRate + '%' +
-                '<br/> <span>' + '최대 할인 금액 ' + coupon.maxDiscountPrice + '원' + '</span>')
-            + '</td>'
-            + '<td style="color: dodgerblue">' + (coupon.couponCodeId == 1 ? coupon.discountPrice + '원' : coupon.discountRate + '%') + '</td>'
-            + '</tr>';
+
+        let priceRow;
+        if (coupon.couponCodeId == 1) {
+            priceRow = '<td>' + coupon.discountPrice + '원' + '<br/>' + '<span>' + '상품 가격이 최소 ' + coupon.minOrderPrice + '원 이상' + '</span></td>'
+                + '<td style="color: dodgerblue">' + coupon.discountPrice + '원 </td>'
+        } else {
+            priceRow = '<td>' + coupon.discountRate + '%' + '<br/>' + '<span>' + '최대 할인 금액 ' + coupon.maxDiscountPrice + '원' + '</span></td>'
+                + '<td style="color: dodgerblue">' + coupon.discountRate + '% </td>';
+        }
+
+        let row =
+            '<tr>' +
+                '<td><input type="radio" name="coupon" value="' + coupon.id + '"></td>' +
+                '<td>' + coupon.name + '</td>' +
+                priceRow +
+            '</tr>';
         couponTableBody.innerHTML += row;
+
+        console.log(couponTableBody);
     });
 }
