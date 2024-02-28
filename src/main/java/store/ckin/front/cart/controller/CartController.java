82e99246a84712dfd8d9b7c1ff6519ccb96c7fc1
dduneon <dduneon@gmail.com>
2024-02-28 18:14:15 +0900

    [ADD] CartController 전체적인 테스트 로직 작성

--- src/main/java/store/ckin/front/cart/controller/CartController.java
+++ src/main/java/store/ckin/front/cart/controller/CartController.java
@@ -1,16 +1,17 @@
 package store.ckin.front.cart.controller;
 
-import java.util.Objects;
-import java.util.UUID;
+import java.util.List;
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import store.ckin.front.cart.dto.CartItemDto;
+import store.ckin.front.util.RedisUtils;
 
 /**
  * description
@@ -20,16 +21,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/cart")
 public class CartController {
+    private final RedisUtils redisUtils;
     @GetMapping
-    public String getCartPage(@CookieValue(name = "CART_ID") Cookie cookie) {
-        log.info("saved cookie: {} {}", cookie.getName(), cookie.getValue());
+    public String getCartPage(@CookieValue(name = "CART_ID") Cookie cookie, Model model) {
+        log.info("getCartPage(): cookie -> {}", cookie.getValue());
+        List<CartItemDto> cart = redisUtils.getCartItems(cookie.getValue());
+        for(CartItemDto cartItem: cart) {
+            log.info("getCartPage(): cartItem -> {}", cartItem.getName());
+        }
+        model.addAttribute(cart);
         return "cart/index";
     }
 
-    @PostMapping
-    public String addCartItem() {
-        return "cart/index";
+    @GetMapping("/create")
+    public String addCartItem(@CookieValue(name = "CART_ID") Cookie cookie) {
+        log.info("addCartPage(): cookie -> {}", cookie.getValue());
+        redisUtils.addCartItem(cookie.getValue(), new CartItemDto("test", 1L, 1, 10000));
+        List<CartItemDto> cart = redisUtils.getCartItems(cookie.getValue());
+        for(CartItemDto cartItem: cart) {
+            log.info("getCartPage(): cartItem -> {}", cartItem.getName());
+        }
+        return "redirect:/cart";
+    }
+
+    @GetMapping("/order")
+    public String proceedOrder(RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute(List<>)
+
+        return "redirect:/sale";
     }
 }
