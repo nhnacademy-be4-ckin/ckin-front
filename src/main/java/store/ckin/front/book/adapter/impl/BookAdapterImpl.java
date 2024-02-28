3949a6dff4723609a5e220cb6959673aca3911bd
f1v3-dev <f1v3@kakao.com>
2024-02-28 14:21:53 +0900

    [FEAT] 도서 - 어댑터 구현

---
+++ src/main/java/store/ckin/front/book/adapter/impl/BookAdapterImpl.java
@@ -0,0 +1,58 @@
+package store.ckin.front.book.adapter.impl;
+
+import static store.ckin.front.util.AdapterHeaderUtil.getHttpHeaders;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import store.ckin.front.book.adapter.BookAdapter;
+import store.ckin.front.book.dto.response.BookSaleResponseDto;
+import store.ckin.front.config.GatewayProperties;
+
+/**
+ * 도서 어댑터 구현 클래스입니다.
+ *
+ * @author 정승조
+ * @version 2024. 02. 28.
+ */
+
+@Component
+@RequiredArgsConstructor
+public class BookAdapterImpl implements BookAdapter {
+
+    private final RestTemplate restTemplate;
+
+    private final GatewayProperties gatewayProperties;
+
+    private static final String BOOK_URI = "/api/books";
+
+    @Override
+    public List<BookSaleResponseDto> requestBookSaleList(List<Long> request) {
+
+        HttpEntity<List<BookSaleResponseDto>> requestEntity = new HttpEntity<>(getHttpHeaders());
+
+
+        ResponseEntity<List<BookSaleResponseDto>> exchange = restTemplate.exchange(
+                gatewayProperties.getGatewayUri() + BOOK_URI + "/extraction?" + buildBookIds(request),
+                HttpMethod.GET,
+                requestEntity,
+                new ParameterizedTypeReference<>() {
+                });
+
+
+        return exchange.getBody();
+    }
+
+    public String buildBookIds(List<Long> bookIds) {
+        return bookIds.stream()
+                .map(bookId -> "bookId=" + bookId)
+                .collect(Collectors.joining("&"));
+    }
+
+}
