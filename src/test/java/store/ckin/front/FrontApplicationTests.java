ec624c46a66a844271753f17cc1f0ea93931202f
gaeun <gaeun@gan-ui-noteubug.local>
2024-02-29 17:07:24 +0900

    [REFACTOR] 주석 제거

--- src/test/java/store/ckin/front/FrontApplicationTests.java
+++ src/test/java/store/ckin/front/FrontApplicationTests.java
@@ -3,13 +3,11 @@ package store.ckin.front;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.UUID;
-
 @SpringBootTest
 class FrontApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
