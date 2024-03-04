8835e379957a26a4635ed0cf423623d43c4829ac
jinwoo Lee <jinwoolee@jinwooui-noteubug.local>
2024-03-04 13:04:47 +0900

    [FEAT] MemberServie : getMemberInfoDetail

--- src/main/java/store/ckin/front/member/service/MemberService.java
+++ src/main/java/store/ckin/front/member/service/MemberService.java
@@ -1,6 +1,8 @@
 package store.ckin.front.member.service;
 
 import store.ckin.front.member.domain.request.MemberCreateRequestDto;
+import store.ckin.front.member.domain.request.MemberInfoDetailRequestDto;
+import store.ckin.front.member.domain.response.MemberInfoDetailResponseDto;
 
 /**
  * Member 관련 서비스에 대한 Interface 입니다.
@@ -10,4 +12,6 @@ import store.ckin.front.member.domain.request.MemberCreateRequestDto;
  */
 public interface MemberService {
     void createMember(MemberCreateRequestDto memberCreateRequestDto);
+
+    MemberInfoDetailResponseDto getMemberInfoDetail(MemberInfoDetailRequestDto memberInfoDetailRequestDto);
 }
