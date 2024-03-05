8c57459232436ccec00f88662d4c31d7b7abdaa8
jinwoo Lee <jinwoolee@jinwooui-noteubug.local>
2024-03-05 18:22:46 +0900

    [REFACTOR] 이름 수정

--- src/main/java/store/ckin/front/member/filter/JwtFilter.java
+++ src/main/java/store/ckin/front/member/filter/JwtFilter.java
@@ -18,7 +18,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
-import store.ckin.front.exception.CookieNouFoundException;
+import store.ckin.front.exception.CookieNotFoundException;
 import store.ckin.front.member.domain.response.MemberInfoDetailResponseDto;
 import store.ckin.front.member.service.MemberDetailsService;
 import store.ckin.front.token.domain.TokenAuthRequestDto;
