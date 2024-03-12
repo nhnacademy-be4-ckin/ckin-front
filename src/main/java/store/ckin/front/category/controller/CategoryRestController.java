54c9bda88e02f0c82c675abb3997bee17e5d9f53
nayoseb <nayoseb1018@gmail.com>
2024-03-12 14:47:50 +0900

    [DOCS] CategoryController javadoc 작성 및 엔드포인트 수정

--- src/main/java/store/ckin/front/category/controller/CategoryRestController.java
+++ src/main/java/store/ckin/front/category/controller/CategoryRestController.java
@@ -20,7 +20,6 @@ import store.ckin.front.category.service.CategoryService;
  * @author 나국로
  * @version 2024. 03. 06.
  */
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/categories")
@@ -29,31 +28,48 @@ public class CategoryRestController {
     private final CategoryService categoryService;
 
 
+    /**
+     * 새로운 카테고리를 생성합니다.
+     *
+     * @param categoryCreateRequestDto 카테고리 생성 요청 DTO
+     */
     @PostMapping("/admin")
     public void createCategory(@RequestBody CategoryCreateRequestDto categoryCreateRequestDto) {
         categoryService.createCategory(categoryCreateRequestDto);
     }
 
-    @GetMapping("/subcategories/{parentId}")
-    public List<CategoryResponseDto> getSubcategories(@PathVariable Long parentId) {
-        return categoryService.getSubcategories(parentId);
-    }
 
+    /**
+     * 최상위 카테고리 목록을 조회합니다.
+     *
+     * @return 최상위 카테고리 목록
+     */
     @GetMapping("/topCategories")
     public List<CategoryResponseDto> getTopCategories() {
         return categoryService.getTopCategories();
     }
 
+    /**
+     * 지정된 ID의 카테고리를 업데이트합니다.
+     *
+     * @param requestDto 업데이트할 카테고리의 정보가 담긴 DTO
+     * @param categoryId 카테고리 ID
+     */
     @PutMapping("/admin/{categoryId}")
     public void updateCategory(@RequestBody CategoryUpdateRequestDto requestDto, @PathVariable Long categoryId) {
         categoryService.updateCategory(categoryId, requestDto);
     }
 
+
+
+    /**
+     * 지정된 부모 ID에 해당하는 하위 카테고리 목록을 조회합니다.
+     *
+     * @param parentId 부모 카테고리 ID
+     * @return 하위 카테고리 목록
+     */
     @GetMapping("/{parentId}")
-    public List<CategoryResponseDto> getCouponPage(@PathVariable("parentId") Long parentId) {
-
-        List<CategoryResponseDto> categoryList = categoryService.getSubcategories(parentId);
-
-        return categoryList;
+    public List<CategoryResponseDto> getSubcategories(@PathVariable("parentId") Long parentId) {
+        return categoryService.getSubcategories(parentId);
     }
 }
