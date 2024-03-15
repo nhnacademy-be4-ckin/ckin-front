b8ccb6c22c196da01b77f926663d988f6973cf13
nayoseb <nayoseb1018@gmail.com>
2024-03-15 09:55:04 +0900

    [FEATURE] 메인페이지에 보여줄 책 데이터 adapter 구현

--- src/main/java/store/ckin/front/product/adapter/impl/ProductAdapterImpl.java
+++ src/main/java/store/ckin/front/product/adapter/impl/ProductAdapterImpl.java
@@ -1,5 +1,6 @@
 package store.ckin.front.product.adapter.impl;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Pageable;
@@ -13,6 +14,7 @@ import store.ckin.front.config.properties.GatewayProperties;
 import store.ckin.front.coupontemplate.dto.response.PageDto;
 import store.ckin.front.product.adapter.ProductAdapter;
 import store.ckin.front.product.dto.response.BookListResponseDto;
+import store.ckin.front.product.dto.response.BookMainPageResponseDto;
 import store.ckin.front.product.dto.response.BookResponseDto;
 
 import static store.ckin.front.util.AdapterHeaderUtil.getHttpHeaders;
@@ -65,6 +67,43 @@ public class ProductAdapterImpl implements ProductAdapter {
                         requestEntity,
                         new ParameterizedTypeReference<>() {
                         });
+        return exchange.getBody();
+    }
+
+    @Override
+    public List<BookMainPageResponseDto> findRecentBooks(Integer limit) {
+        HttpEntity<Pageable> requestEntity = new HttpEntity<>(getHttpHeaders());
+        String url = UriComponentsBuilder.fromHttpUrl(portProperties.getGatewayUri() + "/api/books/main-page")
+                .queryParam("limit", limit)
+                .encode()
+                .toUriString();
+
+        ResponseEntity<List<BookMainPageResponseDto>> exchange =
+                restTemplate.exchange(url,
+                        HttpMethod.GET,
+                        requestEntity,
+                        new ParameterizedTypeReference<>() {
+                        });
+
+        return exchange.getBody();
+    }
+
+
+    @Override
+    public List<BookMainPageResponseDto> findRecentBooksByCategoryId(Long categoryId, Integer limit) {
+        HttpEntity<Pageable> requestEntity = new HttpEntity<>(getHttpHeaders());
+        String url = UriComponentsBuilder.fromHttpUrl(portProperties.getGatewayUri() + "/api/books/main-page/category/" + categoryId)
+                .queryParam("limit" ,limit)
+                .encode()
+                .toUriString();
+
+        ResponseEntity<List<BookMainPageResponseDto>> exchange =
+                restTemplate.exchange(url,
+                        HttpMethod.GET,
+                        requestEntity,
+                        new ParameterizedTypeReference<>() {
+                        });
+
         return exchange.getBody();
     }
 }
