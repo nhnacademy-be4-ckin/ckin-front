b6bd63d9dcb8cf14fa682b0e0e8c4a4048003c01
jinwoo Lee <jinwoolee@jinwooui-noteubug.local>
2024-03-20 00:35:48 +0900

    [FEAT] 주문 페이지에서 주소 수정 & 삭제

--- src/main/java/store/ckin/front/address/controller/AddressController.java
+++ src/main/java/store/ckin/front/address/controller/AddressController.java
@@ -6,11 +6,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.*;
 import store.ckin.front.address.domain.request.AddressAddRequestDto;
+import store.ckin.front.address.domain.request.AddressUpdateRequestDto;
 import store.ckin.front.address.domain.response.MemberAddressResponseDto;
 import store.ckin.front.address.service.AddressService;
 
@@ -48,8 +46,41 @@ public class AddressController {
      * 주문 페이지에서 주소를 추가할 때 호출되는 메서드 입니다.
      */
     @PostMapping("/sale/address/add")
-    public String addAddress(@Valid @ModelAttribute AddressAddRequestDto addressAddRequestDto) {
+    public String addAddressInSale(@Valid @ModelAttribute AddressAddRequestDto addressAddRequestDto) {
         addressService.addAddress(getMemberId(), addressAddRequestDto);
+
+        return "redirect:/sale/address/list";
+    }
+
+    @GetMapping("/sale/address/update/{addressId}")
+    public String getUpdateAddressInSale(@PathVariable("addressId") Long addressId,
+                                         Model model) {
+        List<MemberAddressResponseDto> addressList = addressService.getMemberAddressList(getMemberId());
+        addressList.stream()
+                .filter(responseDto -> responseDto.getAddressId().equals(addressId))
+                .findFirst()
+                .ifPresent(address -> model.addAttribute("address", address));
+
+        return "address/sale-update";
+    }
+
+    /**
+     * 주문 페이지에서 주소를 수정할 때 호출되는 메서드 입니다.
+     */
+    @PutMapping("/sale/address/update/{addressId}")
+    public String updateAddressInSale(@PathVariable("addressId") Long addressId,
+                                      @Valid @ModelAttribute AddressUpdateRequestDto addressUpdateRequestDto) {
+        addressService.updateAddress(getMemberId(), addressId, addressUpdateRequestDto);
+
+        return "redirect:/sale/address/list";
+    }
+
+    /**
+     * 주문 페이지에서 주소를 제거할 때 호출되는 메서드 입니다.
+     */
+    @DeleteMapping("/sale/address/delete/{addressId}")
+    public String removeAddressInSale(@PathVariable("addressId") Long addressId) {
+        addressService.deleteAddress(getMemberId(), addressId);
 
         return "redirect:/sale/address/list";
     }
