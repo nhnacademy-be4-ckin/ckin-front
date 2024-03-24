87c494571a23f7239b61b75162d71ccaaeec028c
dduneon <dduneon@gmail.com>
2024-03-25 01:44:03 +0900

    [FEAT] BoolQuery로 변경 및 각종 옵션 필터링 적용

--- src/main/java/store/ckin/front/product/repository/ProductSearchRepository.java
+++ src/main/java/store/ckin/front/product/repository/ProductSearchRepository.java
@@ -1,11 +1,13 @@
 package store.ckin.front.product.repository;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.elasticsearch.index.query.MultiMatchQueryBuilder;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
@@ -29,17 +31,38 @@ import store.ckin.front.product.domain.SearchProduct;
 public class ProductSearchRepository {
     private final ElasticsearchOperations operations;
 
-    public PagedResponse<List<SearchProduct>> findProductByKeyword(String keyword, PageRequest pageRequest) {
-        MultiMatchQueryBuilder query =
-                QueryBuilders.multiMatchQuery(keyword).fields(Map.of(
-                                "title.ngram", 5.0f,
-                                "title.nori", 5.0f,
-                                "description.ngram", 1.5f,
-                                "description.nori", 1.5f,
-                                "publisher", 2.0f,
-                                "author", 2.0f
-                        )
+    private QueryBuilder addCategoryQuery(String category) {
+        return QueryBuilders.matchQuery("category", category);
+    }
+
+    private Map<String, Float> getAdditionalFilter(List<String> filters) {
+        Map<String, Float> result = new HashMap<>();
+        for(String filter: filters) {
+            if(filter.equals("title")) {
+                result.put("title.ngram", 5.0f);
+                result.put("title.nori", 5.0f);
+            } else if (filter.equals("description")) {
+                result.put("description.ngram", 1.5f);
+                result.put("description.nori", 1.5f);
+            } else if (filter.equals("publisher")) {
+                result.put("publisher", 2.0f);
+            } else if (filter.equals("author")) {
+                result.put("author", 2.0f);
+            }
+        }
+        return result;
+    }
+
+    public PagedResponse<List<SearchProduct>> findProductByKeyword(String keyword, PageRequest pageRequest, List<String> filters, List<String> categories) {
+        BoolQueryBuilder query = QueryBuilders.boolQuery()
+                .must(QueryBuilders.multiMatchQuery(keyword)
+                        .fields(getAdditionalFilter(filters))
                 );
+
+        for(String category: categories) {
+            query.must(addCategoryQuery(category));
+        }
+
         NativeSearchQuery nativeSearchQuery = new NativeSearchQueryBuilder()
                 .withQuery(query)
                 .build();
@@ -62,4 +85,5 @@ public class ProductSearchRepository {
 
         return new PagedResponse<>(pagedResult, pageInfo);
     }
+
 }
