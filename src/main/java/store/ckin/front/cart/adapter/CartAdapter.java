15fb93e916c7ab3fe344f3783cba5529619803c4
dduneon <dduneon@gmail.com>
2024-03-26 22:02:58 +0900

    [FEAT] CartAdapter 작업

--- src/main/java/store/ckin/front/cart/adapter/CartAdapter.java
+++ src/main/java/store/ckin/front/cart/adapter/CartAdapter.java
@@ -1,5 +1,8 @@
 package store.ckin.front.cart.adapter;
 
+import store.ckin.front.cart.dto.request.CartCreateRequestDto;
+import store.ckin.front.cart.dto.response.CartIdResponseDto;
+
 /**
  * description
  *
@@ -7,4 +10,7 @@ package store.ckin.front.cart.adapter;
  * @version 2024. 02. 27
  */
 public interface CartAdapter {
+    void insertCart(CartCreateRequestDto cartCreateRequestDto);
+
+    CartIdResponseDto selectCartIdByMemberId(Long memberId);
 }
