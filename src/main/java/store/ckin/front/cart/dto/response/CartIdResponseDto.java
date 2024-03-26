9791395059a9c23de69e7e2048a38be1e7fed256
dduneon <dduneon@gmail.com>
2024-03-26 22:03:18 +0900

    [FEAT] CartDto 생성

---
+++ src/main/java/store/ckin/front/cart/dto/response/CartIdResponseDto.java
@@ -0,0 +1,16 @@
+package store.ckin.front.cart.dto.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * description:
+ *
+ * @author : dduneon
+ * @version : 2024. 03. 26
+ */
+@NoArgsConstructor
+@Getter
+public class CartIdResponseDto {
+    private String cartId;
+}
