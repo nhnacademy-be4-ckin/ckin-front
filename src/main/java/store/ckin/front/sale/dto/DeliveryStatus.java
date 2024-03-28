d1f2952e47a0b052d87915075efabf859957e426
f1v3-dev <f1v3@kakao.com>
2024-03-28 14:00:38 +0900

    [REFACTOR] 주문 상세 - 배송 상태, 결제 상태 변경

--- src/main/java/store/ckin/front/sale/dto/DeliveryStatus.java
+++ src/main/java/store/ckin/front/sale/dto/DeliveryStatus.java
@@ -1,13 +1,22 @@
 package store.ckin.front.sale.dto;
 
+import lombok.Getter;
+
 /**
  * 주문의 배송 상태를 나타내는 Enum.
  *
  * @author 정승조
  * @version 2024. 03. 19.
  */
+@Getter
 public enum DeliveryStatus {
-    READY,
-    IN_PROGRESS,
-    DONE
+    READY("배송 준비 중"),
+    IN_PROGRESS("배송 중"),
+    DONE("배송 완료");
+
+    private final String status;
+
+    DeliveryStatus(String status) {
+        this.status = status;
+    }
 }
--- src/main/java/store/ckin/front/sale/dto/SalePaymentStatus.java
+++ src/main/java/store/ckin/front/sale/dto/SalePaymentStatus.java
@@ -1,11 +1,23 @@
 package store.ckin.front.sale.dto;
 
+import lombok.Getter;
+
 /**
  * 주문의 결제 상태를 나타내는 Enum.
  *
  * @author 정승조
  * @version 2024. 03. 19.
  */
+
+@Getter
 public enum SalePaymentStatus {
-    WAITING, PAID, CANCEL
+    WAITING("결제 대기 중"),
+    PAID("결제 완료"),
+    CANCEL("결제 취소");
+
+    private final String status;
+
+    SalePaymentStatus(String status) {
+        this.status = status;
+    }
 }
